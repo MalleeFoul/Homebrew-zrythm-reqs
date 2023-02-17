6ecfba59e7cda4193df876e9a36c1ed2590c6fac
MalleeFoul <120618020+MalleeFoul@users.noreply.github.com>
2023-02-17 13:59:43 +1100

    Update libcyaml.rb

--- Formula/libcyaml.rb
+++ Formula/libcyaml.rb
@@ -1,1 +1,4 @@
-
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Libcyaml < Formula
+  desc "C library for reading and writing YAML. "
