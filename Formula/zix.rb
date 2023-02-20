018264bc07b56ad0bd5cacd0d54ae3a434edbd22
MalleeFoul <120618020+MalleeFoul@users.noreply.github.com>
2023-02-20 12:30:02 +1100

    Added zix.rb

---
+++ Formula/zix.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Zix < Formula
+  desc "A lightweight C99 portability and data structure library"
+  homepage "https://gitlab.com/drobilla/zix"
+  license ""
+  head "https://gitlab.com/drobilla/zix.git"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test zix`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
