2f08a367267d6ac6cc808ac0f8f621d2c1e5eaac
MalleeFoul <120618020+MalleeFoul@users.noreply.github.com>
2023-02-23 20:41:02 +1100

    Update fftw-threads.rb

--- Formula/fftw-threads.rb
+++ Formula/fftw-threads.rb
@@ -27,7 +27,7 @@ depends_on "open-mpi"
 
     # FFTW supports runtime detection of CPU capabilities, so it is safe to
     # use with --enable-avx and the code will still run on all CPUs
-    simd_args = []
+    simd_args = [--enable-threads]
     simd_args += %w[--enable-sse2 --enable-avx --enable-avx2] if Hardware::CPU.intel?
 
     # single precision
