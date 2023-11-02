65faa5c9b78e8c4259f9ab4af72d2e3992d770e4
MalleeFoul <visittheinfidelw.expln.pamphlets@gmail.com>
2023-11-03 09:18:27 +1100

    misc commit

---
+++ Formula/libbacktrace.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Libbacktrace < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ianlancetaylor/libbacktrace/archive/refs/heads/master.zip"
+  version "1.0"
+  sha256 "8901989838c63fcf3d6eadfc9fd825f8383f3624809147f898c6821b4125bc3c"
+  license ""
+
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libbacktrace`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
